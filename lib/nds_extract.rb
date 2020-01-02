90239d8ab256c048cb4d3d5126134bb1074409e9
Dino Kunovac <dinokunovac@gmail.com>
2020-01-02 15:03:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,7 +23,7 @@ def gross_for_director(director_data)
   i = 0
   
   while i < director_data[:movies].length do
-    total += director_data[:movies][i][:worldwide_grosses]
+    total += director_data[:movies][i][:worldwide_gross]
     i += 1
   end
   total
