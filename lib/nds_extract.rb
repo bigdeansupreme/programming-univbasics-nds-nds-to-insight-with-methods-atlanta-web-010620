b7bac78f804e8fa85e71d121145e546146887757
Dino Kunovac <dinokunovac@gmail.com>
2020-01-02 14:57:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,4 +19,8 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  total = 0
+  i = 0
+  
+  
 end
